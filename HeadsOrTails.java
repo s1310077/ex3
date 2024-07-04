c413c1c3b027cf15144e9e815cdb27ef82a2cf18
s1310077 <s1310077@u-aizu.ac.jp>
2024-07-04 16:24:09 +0900

    Add files via upload

---
+++ HeadsOrTails.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class HeadsOrTailsGame{
+    private enum Coin{Heads,Tails}
+    public static void main(String[] args){
+        Random random=new Random();
+
+        System.out.print("Who are you?\n>");
+        String name =scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+
+        System.out.println("Tossing a coin...");
+        int heads=0;
+        int tails=0;
+
+        for(int i=1; i<=3; i++){
+            Coin result=random.nextBoolean()? Coin.Heads :Coin.Tails;
+            System.out.println("Round" + i +":"+result);
+            if(result==Coin.Heads){
+                heads++;
+            }
+            else{
+                tails++;
+            }
+        }
+        System.out.println("Heads :," + heads +"Tais :"+tails);
+
+        if(heads>tails){
+            System.out.println("You win!")
+        }
+        else{
+            System.out.println("You lost!")
+        }
+    }
+}
