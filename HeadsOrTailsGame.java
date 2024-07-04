3e509bfcf5f686892e8b1d70825bb675380c175a
s1310077 <s1310077@u-aizu.ac.jp>
2024-07-04 14:21:11 +0900

    User name added

---
+++ HeadsOrTailsGame.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class HeadsOrTailsGame{
+    private enum Coin{Heads,Tails}
+    public static void main(String[] args){
+        Random random=new Random();
+
+        System.out.println("Tossing a coin...");
+        int heads=0;
+        int tails=0;
+
+        for(int i=1; i<=3; i++){
+            Coin result=random.nextBoolean()? Coin.Heads :Coin.Tails;
+            System.out.println("Round" + i +":"+result);
+            if(result==Coin.Heads){
+                heads++;
+            }
+            else{
+                tails++;
+            }
+        }
+        System.out.println("Heads :," + heads +"Tais :"+tails);
+    }
+}
